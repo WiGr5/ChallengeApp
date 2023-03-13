f54b153fd0ec8c03529e674bac0f2d7a69fe8694
WiGr5 <pegaz125@o2.pl>
2023-03-13 18:34:11 +0100

    Zadanie D4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex = 'K'; 
+var age = 33;
+
+if (sex == 'K')
+{
+    if (age < 33)
+    {
+        Console.WriteLine("Kobieta poniżej 33 lat");
+    }
+
+    else if (age > 33)
+    {
+        Console.WriteLine("Kobieta powyżej 33 lat");
+    }
+
+    else if (age == 33 && name == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+
+    else
+    {
+        Console.WriteLine("Kobieta lat 33");
+    }
+}
+
+else if (sex == 'M')
+{
+    if(age >= 18)
+    {
+        Console.WriteLine("Pelnoletni Mężczyzna");
+    }
+
+    else
+    {
+        Console.WriteLine("Niepelnoletni Mężczyzna");
+    }
+}
+
+else
+{
+    Console.WriteLine("Nie jesteś Kobietą ani Mężczyzną");
+}
