684d65a8833f9c3c06bab1d1ee4c5718896174c9
WiGr5 <pegaz125@o2.pl>
2023-07-29 21:11:37 +0200

    Dzień 10 Metoda AddGrade z INT double string float

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -1,4 +1,6 @@
-﻿namespace ChallengeApp
+﻿using System.ComponentModel.Design;
+
+namespace ChallengeApp
 {
     public class Employee
     {
@@ -14,10 +16,43 @@
 
         public void AddGrade(float grade)
         {
-            this.grades.Add(grade);
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Grdes is over values 0-100");
+            }
+        }
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+            {
+                this.AddGrade(result);
+            }
+            else
+            {
+                Console.WriteLine("String is not float");
+            }
+        }
+        public void AddGrade(int grade)
+        {
+            float result = grade;
+            this.AddGrade(result);
+        }
+        public void AddGrade(double grade)
+        {
+            float result = (float)grade;
+            this.AddGrade(result);
+        }
+        public void AddGrade(long grade)
+        {
+            float result = (float)grade;
+            this.AddGrade(result);
         }
 
-        public Statistics GetStatistics()
+            public Statistics GetStatistics()
         {
             var statistic = new Statistics();
 
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -3,8 +3,12 @@
 var employee = new Employee("Jan", "Kowalski");
 employee.AddGrade(5);
 employee.AddGrade(4);
-employee.AddGrade(3);
-employee.AddGrade(6);
+employee.AddGrade(3000);
+employee.AddGrade("10");
+employee.AddGrade("Adam");
+employee.AddGrade(11);
+employee.AddGrade(12l);
+
 
 var statistic = employee.GetStatistics();
 Console.WriteLine($"Min: {statistic.Min}");
